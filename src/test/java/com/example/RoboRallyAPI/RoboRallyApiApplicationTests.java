5aa35830a4a9cf320498715f17858cd4430d9c01
valde12 <s224289@DTU.dk>
2023-07-25 17:12:30 +0200

    test

---
+++ src/test/java/com/example/RoboRallyAPI/RoboRallyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoboRallyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboRallyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
