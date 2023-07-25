3195ae1074d326c7e50ad3e23ac4d2ff42ca0aa6
valde12 <113907060+valde12@users.noreply.github.com>
2023-07-25 17:08:04 +0200

    Update GameConfigController.java

--- src/main/java/com/example/RoboRallyAPI/GameConfigController.java
+++ src/main/java/com/example/RoboRallyAPI/GameConfigController.java
@@ -1,11 +1,9 @@
 package com.example.RoboRallyAPI;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 @RestController
 public class GameConfigController {
@@ -18,10 +16,51 @@ public class GameConfigController {
     }
 
     @PostMapping(path = "/Servers")
-    public void newServer(@RequestBody String JSONString, @RequestParam String serverID){
+    public void newServer(@RequestBody String JSONString, @RequestParam String serverID, @RequestParam String playerNumber){
         String path = "Servers/" + serverID;
-        File file = new File(path, "mapConfig.json");
-        writeToJSONFile(JSONString, "");
+        File file = new File("Servers/" + serverID);
+        file.mkdir();
+        File file1 = new File(path, "mapConfig.json");
+        File file2 = new File(path, "playerNumberHandler.txt");
+        try {
+            file1.createNewFile();
+            file2.createNewFile();
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+        writeToJSONFile(JSONString, "Servers/" + serverID + "/mapConfig.json");
+        writeToJSONFile(playerNumber, "Servers/" + serverID + "/playerNumberHandler.txt");
+    }
+
+    @GetMapping(path = "/Servers")
+    public String joinServer(@RequestParam String serverID){
+        String path = "Servers/" + serverID + "/mapConfig.json";
+        return readFromJSONFile(path);
+    }
+    @GetMapping(path = "/Servers/playerNumber")
+    public String getPlayerNumber(@RequestParam String serverID){
+        String path = "Servers/" + serverID + "/playerNumberHandler.txt";
+        int playerNumber = Integer.parseInt(readFromJSONFile(path));
+        if (playerNumber != 1) {
+            writeToJSONFile(String.valueOf(playerNumber-1), path);
+            return String.valueOf(playerNumber-1);
+        } else {
+            return "0";
+        }
+    }
+
+    @PutMapping(path = "/Servers/Player")
+    public void updateSpecificPlayer(@RequestBody String JSONString, @RequestParam String serverID, @RequestParam String playerNumber) {
+        String path = "Servers/" + serverID + "/mapConfig.json";
+        JsonObject mapConfigFromClient = new JsonParser().parse(JSONString).getAsJsonObject();
+        JsonObject mapConfigFromServer = new JsonParser().parse(readFromJSONFile(path)).getAsJsonObject();
+        JsonArray jsonArrayClient = mapConfigFromClient.getAsJsonArray("players");
+        JsonElement clientPlayer = jsonArrayClient.get(Integer.parseInt(playerNumber));
+        System.out.println(clientPlayer);
+        System.out.println(mapConfigFromServer);
+        System.out.println(mapConfigFromClient);
+        mapConfigFromServer.getAsJsonArray("players").set(Integer.parseInt(playerNumber), clientPlayer);
+        writeToJSONFile(mapConfigFromServer.toString(), path);
     }
 
     static void writeToJSONFile(String JSONString, String writeFilePath) {
@@ -32,6 +71,18 @@ public class GameConfigController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    static String readFromJSONFile(String readFilePath) {
+        String JSONString;
+        try (FileReader fileReader = new FileReader(readFilePath)){
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            JSONString = bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return JSONString;
     }
 
 
